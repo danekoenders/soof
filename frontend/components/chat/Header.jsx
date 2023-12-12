e20d4a06f28bd4f0815d1445d1445224319eb0f9
Dane Koenders <dane.koenders@gmail.com>
2023-12-12 10:20:42 +0100

    Updated

--- frontend/components/chat/Header.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-export default function Header(props) {
-
-  return <div className="header" style={{backgroundColor: props.backgroundColor}}>&nbsp;123vitamines.nl</div>;
-}
